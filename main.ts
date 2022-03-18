ba9501f3498b8121ddde6480acabcf5cf06faf0b
igarralmat <93262344+igarralmat@users.noreply.github.com>
2022-03-18 14:04:17 +0100

    Actualizar main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,5 +1,5 @@
 DFRobotMaqueenPlus.I2CInit()
 basic.forever(function () {
-    DFRobotMaqueenPlus.mototRun(Motors.M1, Dir.CW, 0)
-    DFRobotMaqueenPlus.mototRun(Motors.M2, Dir.CW, 0)
+    DFRobotMaqueenPlus.mototRun(Motors.M1, Dir.CW, 45)
+    DFRobotMaqueenPlus.mototRun(Motors.M2, Dir.CW, 20)
 })
